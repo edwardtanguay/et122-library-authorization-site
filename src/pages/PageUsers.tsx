9ce65ab8a5d1269ba115d3bf8035838fbe92764f
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-19 13:45:38 +0100

    users page only visible by admins

--- src/pages/PageUsers.tsx
+++ src/pages/PageUsers.tsx
@@ -1,17 +1,22 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { User } from "../components/User";
+import * as tools from "../tools";
 
 export const PageUsers = () => {
-	const { users } = useContext(AppContext);
+	const { users, currentUser } = useContext(AppContext);
 	return (
 		<>
-			<p className="mb-4">There are {users.length} users:</p>
-			<section className="flex gap-2 flex-wrap">
-				{users.map((user) => {
-					return <User user={user} key={user._id} />;
-				})}
-			</section>
+			{tools.isMemberOfAccessGroup(currentUser, "administrators") ? (
+				<>
+					<p className="mb-4">There are {users.length} users:</p>
+					<section className="flex gap-2 flex-wrap">
+						{users.map((user) => {
+							return <User user={user} key={user._id} />;
+						})}
+					</section>
+				</>
+			) : <div>no access</div>}
 		</>
 	);
 };
